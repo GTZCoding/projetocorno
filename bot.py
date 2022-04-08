6a67a479a5a783e0bd7c976a5de462fcc306abb5
Daniel Reis <danielxreis@outlook.com>
2022-04-08 17:16:14 -0300

    Bot com Comandos
    Fiz diversas mudanças para o bot.py ter conexão com o Discord e aceitar comandos

--- bot.py
+++ bot.py
@@ -1,5 +1,26 @@
+import os
 
-from datetime import datetime
+from discord.ext import commands
 
+from dotenv import load_dotenv
+load_dotenv()
 
-inicio = perf_counter()
+TOKEN = os.getenv('TOKEN')
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} tá pronto para botar para quebrar!')
+
+# Reconhecer comandos
+@bot.event
+async def on_message(mensagem):
+    if mensagem.content == "pleus":
+        await mensagem.add_reaction('👍')
+
+@bot.command()
+async def plau(ctx, arg):
+    await ctx.send(arg)
+
+bot.run(TOKEN)
