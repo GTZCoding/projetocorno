69060ccccda351ebff4c219362b50963b0a61263
Daniel Reis <danielxreis@outlook.com>
2022-04-09 19:22:42 -0300

    update para bot q fala

--- bot.py
+++ bot.py
@@ -1,33 +1,36 @@
-# bot.py
 import os
-
-import discord
+from discord.utils import get
+from discord.ext import commands
+from discord import FFmpegPCMAudio
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} se conectou no Discord!')
+    print(f'{bot.user} se conectou no Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    MensagemDoBot1 = 'Olá, eu sou o C.O.R.N.O!'
-       
-    if message.content == 'Plau':
-        await message.channel.send(MensagemDoBot1)
-    if message.content == 'Pleu':
-        await message.channel.send(f'Não me chame de corno. {MensagemDoBot1}.12*2. {40*15}')
-    if message.content == 'Pliu':
-        await message.channel.send(f'Eu não sei o que é {message.author} mas eu sei o que é {client.user}. Que loucura {message.content}.')
-        print('Que loucura')
-    
-        
-    
-client.run(TOKEN)
+# Comando "!plau"
+# Função do Bot que recebe o link/busca da música, se não enviar parâmetro, ele vai reclamar.
+@bot.command()
+async def plau(ctx, arg = None):
+    if arg == None:
+        await ctx.message.add_reaction("<:Dwight4:942070187920850975>")
+        await ctx.send("Cadê o link, porra?")
+    else:
+        if ctx.author.voice:
+            await ctx.send("Hum... Agora tá tudo certo... Mas... Eu ainda não sei falar música... *Ainda*. ")
+            await ctx.message.add_reaction("<:guedes:702528303772467280>")
+            channel = ctx.author.voice.channel
+            voice = await channel.connect()
+            source = FFmpegPCMAudio("teste.wav")
+            player = voice.play(source)
+        else:
+            await ctx.message.add_reaction("<:Dwight4:942070187920850975>")
+            await ctx.send("Porra, você precisa estar conectado em áudio para ouvir áudio, né filho da puta.")
+
+bot.run(TOKEN)
 
